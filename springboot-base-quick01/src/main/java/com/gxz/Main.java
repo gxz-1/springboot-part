d6599ee12029867652399838b2082ed590c6ef44
gxz <xizhuogao@foxmail.com>
2024-01-08 21:12:25 +0800

    springboot start!

--- springboot-base-quick01/src/main/java/com/gxz/Main.java
+++ springboot-base-quick01/src/main/java/com/gxz/Main.java
@@ -1,7 +1,12 @@
 package com.gxz;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //设置为springboot启动类
+//1.这个类是配置类，可以添加@Bean 2.自动加载其他配置类 3.扫描当前类所在根包下的目录下的Component组件
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class,args);//自动创建ioc容器并启动tomcat服务器
     }
 }
--- springboot-base-quick01/src/main/java/com/gxz/controller/HelloController.java
+++ springboot-base-quick01/src/main/java/com/gxz/controller/HelloController.java
@@ -1,2 +1,15 @@
-package com.gxz.controller;public class HelloController {
+package com.gxz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ResponseBody
+@Controller //在springboot启动类所在根包下的目录自动被扫描
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "hello springboot3!";
+    }
 }
