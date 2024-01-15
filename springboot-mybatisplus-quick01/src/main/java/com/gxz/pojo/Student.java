41daa352bd5ad095ecbc8617e619d28e3f60685e
gxz <xizhuogao@foxmail.com>
2024-01-15 11:41:38 +0800

    设置别名

--- springboot-mybatisplus-quick01/src/main/java/com/gxz/pojo/Student.java
+++ springboot-mybatisplus-quick01/src/main/java/com/gxz/pojo/Student.java
@@ -1,12 +1,16 @@
 package com.gxz.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("students") // 如果与数据库表名不一致，用这个注解映射
 public class Student {
     private Integer id;
     private String name;
     private String gender;
     private Integer age;
+    @TableField("class") // 映射到数据库中的字段
     private String classes;
 }
